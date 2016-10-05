a0201a7618c3b953694debb386a83bc05e0d746f
Andrew Farmer <ahfarmer@gmail.com>
2016-10-04 20:52:05 -0700

    static-layout: render all components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Header from './Header';
+import SearchInput from './SearchInput';
+import EmojiResults from './EmojiResults';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         Welcome to App
+        <Header/>
+        <SearchInput/>
+        <EmojiResults/>
       </div>
     );
   }
--- src/EmojiResults.js
+++ src/EmojiResults.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import EmojiResultRow from './EmojiResultRow';
 
 class EmojiResults extends React.Component {
   render() {
     return (
       <div>
         Welcome to EmojiResults
+        <EmojiResultRow />
       </div>
     );
   }
