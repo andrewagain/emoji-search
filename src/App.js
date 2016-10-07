3f333fdb7d650d6d718ec111cd121cbde6e32890
Andrew Farmer <ahfarmer@gmail.com>
2016-10-06 19:42:26 -0700

    state-and-logic: state changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import Header from './Header';
 import SearchInput from './SearchInput';
 import EmojiResults from './EmojiResults';
-import emojiList from './emojiList.json';
+import filterEmoji from './filterEmoji';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      filteredEmoji: emojiList,
+      filteredEmoji: filterEmoji('', 20),
     };
   }
 
   handleSearchChange = (event) => {
-    console.log('search input changed to:', event.target.value);
+    this.setState({
+      filteredEmoji: filterEmoji(event.target.value, 20),
+    });
   }
 
   render() {
