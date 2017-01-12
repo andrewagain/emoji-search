8a4e760b7686eb75ace66ef6e100bcf964821efb
Alessandro Arnodo <alessandro@arnodo.net>
2017-01-12 17:40:26 +0100

    make search on titles case insesitive

--- src/filterEmoji.js
+++ src/filterEmoji.js
@@ -2,7 +2,7 @@ import emojiList from './emojiList.json';
 
 export default function filterEmoji(searchText, maxResults) {
   return emojiList.filter((emoji) => {
-    if (emoji.title.includes(searchText)) {
+    if (emoji.title.toLowerCase().includes(searchText.toLowerCase())) {
       return true;
     }
     if (emoji.keywords.includes(searchText)) {
