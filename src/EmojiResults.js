b36a1062ae3ea751c30ed47cbb54c3723b322f48
alireza valizade <alirezavalizade0@gmail.com>
2018-02-07 20:14:21 +0330

    fix #7 issue - fix eslint warnings (#8)
    * fix eslint warnings
    
    * Use PureComponent instead of Component

--- src/EmojiResults.js
+++ src/EmojiResults.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import EmojiResultRow from './EmojiResultRow';
 import './EmojiResults.css';
 
-class EmojiResults extends React.Component {
+class EmojiResults extends PureComponent {
   render() {
     return (
       <div className="component-emoji-results">
-        {
-          this.props.emojiData.map((emojiData) => {
-            return (
+        {this.props.emojiData.map((emojiData) =>
               <EmojiResultRow
                 key={emojiData.title}
                 symbol={emojiData.symbol}
                 title={emojiData.title}
               />
-            );
-          })
-        }
+          )}
       </div>
     );
   }
