96009644d956b414abf33287bc3263f947876fa7
Nilesh108 <nkawathekar@gmail.com>
2018-10-22 01:51:59 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/Nilesh108/emoji-search.git') {  node -> // is hudson.plugins.git.GitSCM
+           // node / gitConfigName('DSL User')
+          //  node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
     triggers {
