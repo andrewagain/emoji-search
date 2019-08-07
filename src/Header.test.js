5ddaababe1afc35ecf62aec5b25fa35718a4bdc4
Juan Vasquez <juanthoniovg@gmail.com>
2019-08-07 11:46:53 -0500

    add improving to Header.test.js

--- src/Header.test.js
+++ src/Header.test.js
@@ -3,14 +3,18 @@ import { shallow } from "enzyme"
 import Header from "./Header"
 
 describe('Header component', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<Header />)
+  })
+
   it("renders title", () => {
-    const wrapper = shallow(<Header />)
-    const title   = "Emoji Search"
+    const title = "Emoji Search"
     expect(wrapper.contains(title)).toEqual(true)
   })
 
   it("renders header tag", () => {
-    const wrapper = shallow(<Header />)
     expect(wrapper.exists('.component-header')).toEqual(true)
   })
 });
