1e0a5c7c60331c2d103280a477097934b44fc3f2
Juan Vasquez <juanthoniovg@gmail.com>
2019-08-07 11:03:37 -0500

    add more tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,35 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { shallow, mount } from "enzyme"
 import App from "./App"
+import Header from "./Header"
+import SearchInput from "./SearchInput"
+import EmojiResults from "./EmojiResults"
 
-it("renders App component", () => {
-  shallow(<App />)
+describe('App component', () => {
+  it("renders without crashing", () => {
+    shallow(<App />)
+  })
+
+  it('renders 20 initial emojis', () => {
+    const wrapper = mount(<App />)
+    expect(wrapper.state('filteredEmoji')).toHaveLength(20)
+  })
+
+  describe('should has one', () => {
+    it('Header component', () => {
+      const wrapper = shallow(<App />)
+      expect(wrapper.find(Header)).toHaveLength(1)
+    })
+
+    it('SearchInput component', () => {
+      const wrapper = shallow(<App />)
+      expect(wrapper.find(SearchInput)).toHaveLength(1)
+    })
+
+    it('EmojiResults component', () => {
+      const wrapper = shallow(<App />)
+      expect(wrapper.find(EmojiResults)).toHaveLength(1)
+    })
+  })
 })
+
