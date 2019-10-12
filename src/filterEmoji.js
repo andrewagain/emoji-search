578e24b2d3c3816ad43e297df857793644ad2c07
syed.saad <syed.saad@iomechs.com>
2019-10-13 03:05:24 +0500

    feat(search): search with emoji

--- src/filterEmoji.js
+++ src/filterEmoji.js
@@ -9,7 +9,25 @@ export default function filterEmoji(searchText, maxResults) {
       if (emoji.keywords.includes(searchText)) {
         return true;
       }
+      if (emojiUnicode(emoji.symbol) === (emojiUnicode(searchText))) {
+        return true;
+      }
       return false;
     })
     .slice(0, maxResults);
 }
+
+function emojiUnicode (emoji) {
+  let comp;
+  if (emoji.length === 1) {
+      comp = emoji.charCodeAt(0);
+  }
+  comp = (
+      (emoji.charCodeAt(0) - 0xD800) * 0x400
+    + (emoji.charCodeAt(1) - 0xDC00) + 0x10000
+  );
+  if (comp < 0) {
+      comp = emoji.charCodeAt(0);
+  }
+  return comp.toString('16');
+};
