b47d367fc6ffff65eff641ed51e4215979df9334
malibilgin <bilginmuhammedali@gmail.com>
2021-12-11 21:32:33 +0300

    add @testing-library and header render test

--- src/Header.js
+++ src/Header.js
@@ -2,23 +2,13 @@ import React, { PureComponent } from "react";
 import "./Header.css";
 
 export default class Header extends PureComponent {
-  render() {
-    return (
-      <header className="component-header">
-        <img
-          src="//cdn.jsdelivr.net/emojione/assets/png/1f638.png"
-          width="32"
-          height="32"
-          alt=""
-        />
-        Emoji Search
-        <img
-          src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
-          width="32"
-          height="32"
-          alt=""
-        />
-      </header>
-    );
-  }
+      render() {
+            return (
+                  <header className="component-header">
+                        <img src="//cdn.jsdelivr.net/emojione/assets/png/1f638.png" width="32" height="32" alt="" />
+                        Emoji Search
+                        <img src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png" width="32" height="32" alt="" />
+                  </header>
+            );
+      }
 }
---
+++ src/Header.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import Header from "./Header";
+
+test("header test", () => {
+      render(<Header />);
+});
