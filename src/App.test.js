dfbe402320390220c78e4928eb81c550c67c53b0
malibilgin <bilginmuhammedali@gmail.com>
2021-12-11 21:50:10 +0300

    emoji list rendered succes-20

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
+import { render, screen } from "@testing-library/react";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+      const div = document.createElement("div");
+      render(<App />, div);
+});
+
+it("emoji list rendered succes", () => {
+      render(<App />);
+      const items = screen.getAllByText(/Click to copy emoji/i);
+      expect(items.length).toEqual(20);
 });
--- src/Header.test.js
+++ src/Header.test.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import Header from "./Header";
 
-test("header test", () => {
+it("header test", () => {
       render(<Header />);
 });
