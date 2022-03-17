35f92635bf015778239a6640f0eac817f9d0dc8a
wmt-krunald <krunald@webmob.tech>
2022-03-17 14:12:56 +0530

    Google Search

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ export default class Header extends PureComponent {
           height="32"
           alt=""
         />
-        Emoji Search
+        Google Search
         <img
           src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
           width="32"
